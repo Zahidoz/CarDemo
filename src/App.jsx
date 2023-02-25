257a07741916a349a72b86f126afed9e675b99d5
SinanChiragov <108178237+tuncay-09@users.noreply.github.com>
2023-02-25 16:48:01 +0400

    'salam'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.scss';
+import './styles/reset.scss'
+import "./styles/global.scss";
+
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './nav.scss'
+
+import Filter from '../../asserts/icon/filter.svg'
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div className="nav-left">
+          <h1>MORENT</h1>
+          <div className="inpt-c">
+            
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav
